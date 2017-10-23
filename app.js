745a71789e370e30020d9de8dab3b18d2a17c1a0
yangda2000 <yangda2000@nate.com>
2017-10-23 15:18:03 +0900

    pushpush

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('안녕지환아\n');
+  res.end('도롱뇽\n');
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
