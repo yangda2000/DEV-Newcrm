62d10fba37eb290b49f80023a5cb3654b577c763
yangda2000 <yangda2000@nate.com>
2017-11-02 17:42:15 +0900

    add,update,delete REST api

--- router/main.js
+++ router/main.js
@@ -21,5 +21,93 @@ module.exports = function(app, fs)
             res.json(users[req.params.username]);
        });
     });
+    //세번째 API: POST addUser/:username
+    app.post('/addUser/:username', function(req, res){
 
+        var result = {  };
+        var username = req.params.username;
+
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        // LOAD DATA & CHECK DUPLICATION
+        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+            var users = JSON.parse(data);
+            if(users[username]){
+                // DUPLICATION FOUND
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
+
+            // ADD TO DATA
+            users[username] = req.body;
+
+            // SAVE DATA
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result = {"success": 1};
+                res.json(result);
+            })
+        })
+    });
+    //네번째 API: PUT updateUser/:username
+    app.put('/updateUser/:username', function(req, res){
+
+        var result = {  };
+        var username = req.params.username;
+
+        // CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        // LOAD DATA
+        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+            var users = JSON.parse(data);
+            // ADD/MODIFY DATA
+            users[username] = req.body;
+
+            // SAVE DATA
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result = {"success": 1};
+                res.json(result);
+            })
+        })
+    });
+    //마지막 API: DELETE deleteUser/:username
+    app.delete('/deleteUser/:username', function(req, res){
+        var result = { };
+        //LOAD DATA
+        fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+            var users = JSON.parse(data);
+
+            // IF NOT FOUND
+            if(!users[req.params.username]){
+                result["success"] = 0;
+                result["error"] = "not found";
+                res.json(result);
+                return;
+            }
+
+            delete users[req.params.username];
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result["success"] = 1;
+                res.json(result);
+                return;
+            })
+        })
+
+    })
 }
