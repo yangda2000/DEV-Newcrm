3ad34a3ffbad984d177be343096dfb60fb2f2728
yangda2000 <yangda2000@gmail.com>
2017-11-16 13:49:30 +0900

    port modify

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var fs = require("fs")
@@ -9,11 +10,16 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 
-var server = app.listen(80, function(){
- console.log("Express server has started on port 80")
+var server = app.listen(3000, function(){
+ console.log("Express server has started on port 3000")
 });
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'bnnland')));
+// 가상디렉토리 app.use('/static', express.static('public'));
+//app.use('/static', express.static(__dirname + '/public'));
+//console.log(__dirname);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
